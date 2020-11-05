34ea28df11548e72d8288add628c95902286db4b
Ethan Shen <ethanshen1214@gmail.com>
2020-11-04 19:08:20 -0600

    added the ability to extract the titles on the page

--- scrapy-practice/scraper.py
+++ scrapy-practice/scraper.py
@@ -3,3 +3,13 @@ import scrapy
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2020']
+
+    def parse(self, response):
+        SET_SELECTOR = '.set'
+        for legoset in response.css(SET_SELECTOR):
+
+            NAME_SELECTOR = 'h1 ::text'
+            yield {
+                'name': legoset.css(NAME_SELECTOR).extract_first()
+            }
+
